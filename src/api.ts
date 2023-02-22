7d263018423fb6ea55d817082e8a9623868764ab
wopian <wopian@wopian.me>
2023-02-22 03:29:09 +0000

    style: run linter

--- src/api.ts
+++ src/api.ts
@@ -3,5 +3,5 @@ import ky from 'ky-universal'
 const API_URL = 'https://api.zeepkist-gtr.com/'
 
 export const api: typeof ky = ky.create({
-  prefixUrl: API_URL,
+  prefixUrl: API_URL
 })
--- src/utils/stringifyObjectValues.spec.ts
+++ src/utils/stringifyObjectValues.spec.ts
@@ -3,15 +3,18 @@ import test from 'ava'
 import { stringifyObjectValues } from './index.js'
 
 test('todo', t => {
-  t.deepEqual(stringifyObjectValues({
-    string: 'string',
-    number: 1,
-    boolean: true,
-    array: ['1', 2, true]
-  }), {
-    string: 'string',
-    number: '1',
-    boolean: 'true',
-    array: '1,2,true'
-  })
+  t.deepEqual(
+    stringifyObjectValues({
+      string: 'string',
+      number: 1,
+      boolean: true,
+      array: ['1', 2, true]
+    }),
+    {
+      string: 'string',
+      number: '1',
+      boolean: 'true',
+      array: '1,2,true'
+    }
+  )
 })
